518c890da189e83d742b98409ebb34dfead3dbaa
Stephen Langdon <stphnlngdncoding@gmail.com>
2016-09-07 09:51:17 -0700

    added endpoints to api

--- index.js
+++ index.js
@@ -6,28 +6,56 @@ const config = require('./config');
 const Browser = require('zombie');
 const client = require('twilio')(config.accountSid, config.authToken);
 
+var scrapeController = {};
+
 const seenListings = {};
 let allListings;
 let freshListings;
 
-setInterval(function() {
-	getListingPids()
-	.then(function(allPids){
-		allListings = allPids;
-		populateCache();
-		freshListings = checkAgainstCache(allListings);
-		console.log("fresh listings", freshListings);
-			let intervalId = setInterval(function(){
-			if (freshListings.length === 0) {
-				clearInterval(intervalId);
-			} else {
-				let firstItem = freshListings.shift();
-				getContentFromPid(firstItem);
-			}
-		}, 2000)
-	})
-}, 1000 * 60 * 1); //last variable is minutes
 
+scrapeController.executeScrapeAndText = function(bool, interval) {
+	if (bool) {
+		console.log("initial scrape");
+		getListingPids()
+			.then(function(allPids){
+				allListings = allPids;
+				populateCache();
+				freshListings = checkAgainstCache(allListings);
+				console.log("fresh listings", freshListings);
+				let intervalId = setInterval(function(){
+					if (freshListings.length === 0) {
+						clearInterval(intervalId);
+					} else {
+						let firstItem = freshListings.shift();
+						getContentFromPid(firstItem);
+					}
+				}, 1000)
+			})
+			console.log("setting timer...")
+		var firstInterval = setInterval(function() {
+			getListingPids()
+			.then(function(allPids){
+				allListings = allPids;
+				populateCache();
+				freshListings = checkAgainstCache(allListings);
+				console.log("fresh listings", freshListings);
+				let intervalId = setInterval(function(){
+					if (freshListings.length === 0) {
+						clearInterval(intervalId);
+					} else {
+						let firstItem = freshListings.shift();
+						getContentFromPid(firstItem);
+					}
+				}, 2000)
+			})
+		}, 1000 * 60 * 2); //last variable is minutes
+		return firstInterval;
+	} else {
+		console.log("stopping...");
+		clearInterval(interval);
+	}
+
+}
 
 // let freshListings = checkAgainstCache(listingPids);
 // console.log(freshListings);
@@ -59,67 +87,32 @@ function populateCache() {
 
 function getListingPids() {
 	let prom = new Promise(function(resolve, reject) {
-		Browser.visit('https://sfbay.craigslist.org/search/sfc/apa', (e, browser) => {
+		let url = 'https://'+ config.location + '.craigslist.org/search/sfc/apa'
+		Browser.visit(url, (e, browser) => {
 			// if (e) console.log("error with browser.visit",e);
-			// console.log(browser._windowInScope._response.body);
-			//srch_sfbay.craigslist.org/search/sfc/apa
-			// console.log(browser.localStorage('https://sfbay.craigslist.org/search/sfc/apa').getItem('srch_sfbay.craigslist.org/search/sfc/apa'))
 			let pTags = Array.from(browser.querySelectorAll('p[data-pid]'));
-			// console.log(pTags[0]._attributes[1])
-			// console.log(pTags[0]._attributes['data-pid']._nodeValue)
 			let pids = pTags.map((tag) => {
 				return tag._attributes['data-pid']._nodeValue;
 			})
-
 			resolve(pids);
 		})
 	})
-	return prom
+	return prom;
 }
-
-
-function parsePids (error, response, body) {
-	console.log("error", error);
-	let $ = cheerio.load(body);
-	let listings = $('p.row');
-	let newListings = [];
-	listings.each(function(i) {
-		let pid = $(this).attr("data-pid")
-		if (seenListings[pid] === undefined) {
-			newListings.push(pid);
-			seenListings[pid] = true;
-			fs.appendFileSync('listings.txt', pid + ",");
-
-		}
-	});
-
-	let intervalId = setInterval(function(){
-		if (newListings.length === 0) {
-			clearInterval(intervalId);
-		} else {
-			let firstItem = newListings.shift();
-			getContentFromPid(firstItem);
-		}
-	}, 2000)
-}
-
 
 function getContentFromPid(pid) {
-	// console.log(pid);
 	request.get('https://sfbay.craigslist.org/sfc/apa/' + pid + '.html', function(error, response, body) {
-		console.log("error", error);
+		if (error) console.log("error in getContentFromPid")
 		let $ = cheerio.load(body);
 		let post = {};
 		post.price = $('span.price').text();
-		// post.brs = $('span#housing').text();
 		post.title = $('span#titletextonly').text();
 		post.location = $('small').text();
 		post.url = 'https://sfbay.craigslist.org/sfc/apa/' + pid + '.html';
-		// console.log(post);
 		let postString = post.price + " " + post.title + " " + post.url;
 		console.log(postString);
-		
-		sendSms(postString);
+
+		// sendSms(postString);
 	})
 }
 
@@ -130,3 +123,5 @@ function sendSms(message) {
 		from: config.sendingNumber
 	});
 }
+
+module.exports = scrapeController;
--- server.js
+++ server.js
@@ -1,9 +1,30 @@
+"use strict";
 const express = require('express');
 const app = express();
+const scrapeController = require('./index');
+
+let scrapeInterval;
+
+app.get('/go', (req, res) => {
+	scrapeInterval = scrapeController.executeScrapeAndText(true);
+	return res.send("beginning scrape");
+});
+
+app.get('/stop', (req, res) => {
+	// console.log("scrapeInterval:", scrapeInterval);
+	if (!scrapeInterval) {
+		console.log("you didnt start scraping yet!")
+		return res.send("scraping is not active");
+	} else {
+		scrapeController.executeScrapeAndText(false, scrapeInterval);
+		scrapeInterval = null;
+		return res.send("ending scrape");
+	}
+})
 
 app.get('*', (req, res) => {
-	console.log('hello world!')
-	res.end();
+	console.log('invalid endpoint')
+	return res.end();
 })
 
 app.listen(3000);
